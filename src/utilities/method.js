1b9ec60f7cd09117416db8ceb7bd6340ef91c95b
ADATSENG <d40228112@gmail.com>
2022-11-01 16:30:51 +0800

    change react-dom version

---
+++ src/utilities/method.js
@@ -0,0 +1,15 @@
+
+export const getBackGroundImage = () => {
+  return require(`../images/background_image.jpg`)
+}
+
+
+export const CIRCLE = {
+  BRUSH: 30
+}
+
+
+export const PADDING = {
+  HORIZONTAL: 40,
+  VERTICAL: 20
+}
